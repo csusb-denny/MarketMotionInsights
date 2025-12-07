643e5f18a52c5104d391031201f0461e50cd1ac4
Denny Le <008531881@coyote.csusb.edu>
2025-12-06 22:42:22 -0800

    Finished VWAP function for client request

--- app/main.py
+++ app/main.py
@@ -15,7 +15,22 @@ def root():
 #route function runs:
 @app.get("/vwap")
 def get_vwap(symbol: str):
-    return calculated_vwap(symbol)
+    """
+    candles: list of dicts with keys ['high', 'low', 'close', 'volume']
+    """
+    total_PriceVolume = 0 #Sum of (Typical Price * Volume)
+    Volume = 0      #Sum of Volume
+
+    for c in candles:
+        typical_price = (c['high'] + c['low'] + c['close']) / 3
+        total_Volume += c['volume']
+
+        total_PriceVolume += typical_price * Volume 
+        total_Volume += Volume
+    
+    if total_Volume == 0:
+        return None # Avoid division by zero
+    return total_PriceVolume / total_Volume
 
 #Step 3: Data Retrieval
 #Fetch recent candle data from: Database
