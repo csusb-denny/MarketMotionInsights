aea1a070999b2dfa0627cd45de83612f2bd4270a
Denny Le <008531881@coyote.csusb.edu>
2025-12-07 14:16:00 -0800

    added generate candles endpoint

--- app/main.py
+++ app/main.py
@@ -1,17 +1,27 @@
 from fastapi import FastAPI
 from app.indicators.vwap import calculate_vwap
+from app.utils.candle_generator import generate_candles
 
 app = FastAPI()
 
-#2. Root Endpointer
+#1. Root Endpointer
 @app.get("/")
 def root():
     return {"message": "Market Analysis API is running."}
 
-# 3. VWAP ENDPOINT
+#2. VWAP ENDPOINT
 @app.post("/vwap")
 def vwap_endpoint(candles: list[dict]):
     """
     Calculate VWAP from provided candle data
     """ 
     return {"vwap": calculate_vwap(candles)}
+
+#3. Candle Generation Endpoint
+@app.get("/generate_candles")
+def generate(count: int = 10):
+    """
+    Generate synthetic candle data
+    """ 
+    candles = generate_candles(count)
+    return {"candles": candles}
